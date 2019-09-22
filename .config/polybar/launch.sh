8e858e04d8393fad9a4047f9b2b982031a461f0d
Falcz <lahner.m@gmail.com>
2019-09-22 19:38:33 +0000

    Fixed polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar top_bar &
+polybar bottom_bar &
 
 echo "Bars launched..."
