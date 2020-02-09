6583ce1abde33bd9d289d67b18eeda0b18f6f808
Falcz <lahner.m@gmail.com>
2020-02-09 11:01:44 +0000

    Small spacemacs package configuration change, alonf with an alias or 2.

--- sourced/aliases.sh
+++ sourced/aliases.sh
@@ -1,1 +1,2 @@
 alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
+alias hextoi="printf '%d\n'"
--- sourced/env.sh
+++ sourced/env.sh
@@ -1,5 +1,6 @@
 # Path Configuration
 PATH=${PATH}:$HOME/bin
+PATH=${PATH}:$HOME/.cargo/bin
 export PATH=${PATH}
 
 XDG_CONFIG_HOME="$HOME/.config"
