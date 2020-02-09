83b8dbc0d4f00889601ba8900475450a5d5d3d37
Falcz <lahner.m@gmail.com>
2020-02-09 11:10:50 +0000

    Added basic bootstrap script - incomplete.

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+# AUR Helper
+git clone https://aur.archlinux.org/yay.git ~/.yay
+cd ~/.yay
+makepkg -si
+
+# Shell setup
+sudo pacman -S zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo pacman -S kitty
+
+
+# Spacemacs setup
+sudo pacman -S emacs
+rm -r ~/.emacs.d
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# xinit/i3 setup
+sudo pacman -S xorg-xinit
+sudo pacman -S compton
+sudo pacman -S i3-gaps
+sudo yay -S polybar
+sudo paman -S rofi
+sudo pacman -S feh
+sudo pacman -S betterlockscreen
+
+# Utils
+sudo pacman -S ranger
+sudo pacman -S thefuck
--- sourced/aliases.sh
+++ sourced/aliases.sh
@@ -1,2 +1,4 @@
 alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
 alias hextoi="printf '%d\n'"
+alias vi ='vim'
+
