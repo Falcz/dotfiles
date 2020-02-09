3ca61dbaa4f6b3f7f3707e1b0d7704adde7e25c7
Falcz <lahner.m@gmail.com>
2020-02-09 10:05:41 -0500

    Updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo pacman -S kitty
 
 
 # Spacemacs setup
@@ -20,4 +21,9 @@ sudo pacman -S xorg-xinit
 sudo pacman -S compton
 sudo pacman -S i3-gaps
 sudo yay -S polybar
+sudo paman -S rofi
+sudo pacman -S feh
+sudo pacman -S betterlockscreen
 
+# Utils
+sudo pacman -S ranger
